a89ff60ce3996c952ccbf6edae3ee60240583450
Z Alos <satya123m4@gmail.com>
2025-02-10 19:25:41 +0530

    Optimized::Preset Storing & Retrieving

--- content.js
+++ content.js
@@ -1,6 +1,24 @@
 console.log("nani..??")
 
 const LOCALSTORAGEPREFIX = "WHATSAPP_CUSTOM_THEME_";
+const cssVar = {
+    sidePane: "--side-pane",
+    msgIn: "--msg-in",
+    msgOut: "--msg-out",
+    bars: "--bars",
+    someMoreBars: "--some-more-bars",
+    msgInInner: "--msg-in-inner",
+    msgOutInner: "--msg-out-inner",
+    msgInText: "--msg-in-text",
+    sidePanePrimaryText: "--side-pane-primary-text",
+    sidePaneSecondaryText: "--side-pane-secondary-text",
+    msgOutText: "--msg-out-text",
+    msgInLink: "--msg-in-link",
+    msgOutLink: "--msg-out-link",
+    chatBackground: "--chat-background"
+}
+
+//retrieve preset::local storage
 try {
     const retrievedPreset = JSON.parse(localStorage.getItem(`${LOCALSTORAGEPREFIX}PRESET`))
     if(retrievedPreset) setTheme(retrievedPreset)
@@ -19,43 +37,26 @@ chrome.runtime.onMessage.addListener((message) => {
     }
 
     if(message.apply){
-        const rootStyles = getComputedStyle(document.documentElement);
-        const preset = {
-            sidePane: rootStyles.getPropertyValue("--side-pane").trim(),
-            msgIn: rootStyles.getPropertyValue("--msg-in").trim(),
-            msgOut: rootStyles.getPropertyValue("--msg-out").trim(),
-            bars: rootStyles.getPropertyValue("--bars").trim(),
-            someMoreBars: rootStyles.getPropertyValue("--some-more-bars").trim(),
-            msgInInner: rootStyles.getPropertyValue("--msg-in-inner").trim(),
-            msgOutInner: rootStyles.getPropertyValue("--msg-out-inner").trim(),
-            msgInText: rootStyles.getPropertyValue("--msg-in-text").trim(),
-            sidePanePrimaryText: rootStyles.getPropertyValue("--side-pane-primary-text").trim(),
-            sidePaneSecondaryText: rootStyles.getPropertyValue("--side-pane-secondary-text").trim(),
-            msgOutText: rootStyles.getPropertyValue("--msg-out-text").trim(),
-            msgInLink: rootStyles.getPropertyValue("--msg-in-link").trim(),
-            msgOutLink: rootStyles.getPropertyValue("--msg-out-link").trim(),
-            chatBackground: rootStyles.getPropertyValue("--chat-background").trim()
-        }
-
-        console.log("Saving Changes...")
-        localStorage.setItem(`${LOCALSTORAGEPREFIX}PRESET`, JSON.stringify(preset));
+        applyPreset();
     }
 });
 
 function setTheme(retrievedPreset){
     const target = document.documentElement.style;
-    target.setProperty('--side-pane', retrievedPreset.sidePane);
-    target.setProperty('--msg-in', retrievedPreset.msgIn);
-    target.setProperty('--msg-out', retrievedPreset.msgOut);
-    target.setProperty('--bars', retrievedPreset.bars);
-    target.setProperty('--some-more-bars', retrievedPreset.someMoreBars);
-    target.setProperty('--msg-in-inner', retrievedPreset.msgInInner);
-    target.setProperty('--msg-out-inner', retrievedPreset.msgOutInner);
-    target.setProperty('--side-pane-primary-text', retrievedPreset.sidePanePrimaryText);
-    target.setProperty('--side-pane-secondary-text', retrievedPreset.sidePaneSecondaryText);
-    target.setProperty('--msg-in-text', retrievedPreset.msgInText);
-    target.setProperty('--msg-out-text', retrievedPreset.msgOutText);
-    target.setProperty('--msg-in-link', retrievedPreset.msgInLink);
-    target.setProperty('--msg-out-link', retrievedPreset.msgOutLink);
-    target.setProperty('--chat-background', retrievedPreset.chatBackground);
+    Object.keys(cssVar).forEach(key => {
+        target.setProperty(cssVar[key], retrievedPreset[key]);
+    })
+}
+
+function applyPreset(){
+    const preset = {};
+    const rootStyles = getComputedStyle(document.documentElement);
+
+    Object.keys(cssVar).forEach(key => {
+        preset[key] = rootStyles.getPropertyValue(cssVar[key]).trim();
+    });
+
+    alert("Your Changes Have Been Saved Noob...")
+    console.log("Saving Changes...")
+    localStorage.setItem(`${LOCALSTORAGEPREFIX}PRESET`, JSON.stringify(preset));
 }
